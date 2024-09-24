863306a952944a72a4035144049c3aee511e3db7
Rosie <acquitefx@gmail.com>
2024-09-24 14:14:00 +0100

    Allow only running specific examples

--- tools/examples.py
+++ tools/examples.py
@@ -1,9 +1,9 @@
 from subprocess import run, Popen, PIPE
 from os import listdir, remove, kill
-from os.path import isfile, join
+from os.path import isfile, isdir, join
 from time import sleep
 from select import select
-from sys import stdout, stdin
+from sys import stdout, stdin, argv
 
 TIMEOUT = 2 # seconds
 EXAMPLES_PATH = "./examples"
@@ -33,12 +33,13 @@ def read_from_process(process):
             stdout.write(output)
             stdout.flush()
 
-def examples():
-    examples = sorted([f for f in listdir(EXAMPLES_PATH) if isfile(join(EXAMPLES_PATH, f))])
+def examples(kind):
+    path = f"{EXAMPLES_PATH}/{kind}"
+    examples = sorted([f for f in listdir(path) if isfile(join(path, f))])
 
     for example in examples:
         run(["clear"])
-        code = run(["ellec", "-Dtime", "-Clink-flags", "-lraylib", f"./examples/{example}"]).returncode;
+        code = run(["ellec", "-Dtime", "-Clink-flags", "-lraylib", f"./examples/{kind}/{example}"]).returncode;
 
         if code == 0:
             exec_path = f"./{example.replace(".l", "")}";
@@ -75,4 +76,27 @@ def examples():
 # Intended to be run from root dir
 # `python3 tools/examples.py`
 if __name__ == "__main__":
-    examples()
+    kinds = sorted([f for f in listdir(EXAMPLES_PATH) if
+        isdir(join(EXAMPLES_PATH, f)) and f != "resources"]);
+
+    if len(argv) < 2:
+        print(f"No option was provided, exiting...")
+        print(f"Possible options include: {', '.join(kinds)}")
+        exit(1)
+
+    if argv[1] == "help":
+        print(f"To run examples, you should run the following:");
+        print(f"\n\t{argv[0]} <option goes here>\n")
+        print(f"Possible options include: {', '.join(kinds)}")
+        print("You can CTRL+C to skip to the next example")
+        print("CTRL+C twice in rapid succession to exit the program")
+        exit(0)
+
+    kinds.append("help");
+
+    if not (argv[1] in kinds):
+        print(f"Invalid option was provided: {argv[1]}")
+        print(f"Possible options include: {', '.join(kinds)}")
+        exit(1)
+
+    examples(argv[1])
