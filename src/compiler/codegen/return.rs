7d0c755ebdb527ac02043b737745ef1c325d7dfd
Rosie <acquitefx@gmail.com>
2025-03-31 11:43:47 +0100

    feat: [0.5.8] make return node code simpler

--- src/compiler/codegen/return.rs
+++ src/compiler/codegen/return.rs
@@ -8,28 +8,19 @@ use crate::{
 
 impl Codegen<'_> for Return {
     fn compile(self, gen: &mut Compiler, ctx: &CodegenContext<'_>) -> Option<(Type, Value)> {
-        match gen.generate_statement(
+        let res = gen.generate_statement(
             ctx.func,
             ctx.module,
             *self.value,
             ctx.ty.clone(),
             None,
             true,
-        ) {
-            Some((ret_ty, value)) => {
-                if !ctx.func.borrow_mut().manual {
-                    ctx.func
-                        .borrow_mut()
-                        .add_instruction(Instruction::Return(Some((ret_ty, value, self.location))))
-                }
-            }
-            None => {
-                if !ctx.func.borrow_mut().manual {
-                    ctx.func
-                        .borrow_mut()
-                        .add_instruction(Instruction::Return(None))
-                }
-            }
+        );
+
+        if !ctx.func.borrow_mut().manual {
+            ctx.func.borrow_mut().add_instruction(Instruction::Return(
+                res.map(|(ty, val)| (ty, val, self.location)),
+            ))
         }
 
         None
