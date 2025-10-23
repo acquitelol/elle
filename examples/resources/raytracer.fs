75c2e92f4f4425fae451af84d64a654046d8e025
Rosie <acquitefx@gmail.com>
2025-10-24 00:36:06 +0100

    feat: make the spheres out of glass

--- examples/resources/raytracer.fs
+++ examples/resources/raytracer.fs
@@ -10,6 +10,8 @@ struct Sphere {
     vec3 center;
     vec4 color;
     float radius;
+    float ior;
+    float rough;
 };
 
 struct Camera {
@@ -50,6 +52,13 @@ uniform Plane plane;
 
 float rand(vec2 co) {
     return fract(sin(dot(co, vec2(12.9898, 78.233))) * 43758.5453);
+}
+
+vec3 rand_dir(vec2 seed) {
+    float a = rand(seed * 12.34) * 2 * PI;
+    float z = rand(seed * 56.78) * 2.0 - 1.0;
+    float r = sqrt(max(0.0, 1.0 - z * z));
+    return vec3(r * cos(a), r * sin(a), z);
 }
 
 vec3 checkerboard_color(vec3 point) {
@@ -102,7 +111,10 @@ float intersect_plane(Ray ray) {
 
 bool hit_light(Ray ray, out vec4 light_color) {
     for (int i = 0; i < lights_size; ++i) {
-        float t = intersect_sphere(Ray(ray.position, normalize(lights[i].position - ray.position)), Sphere(lights[i].position, lights[i].color, LIGHT_RADIUS));
+        float t = intersect_sphere(
+            Ray(ray.position, normalize(lights[i].position - ray.position)),
+            Sphere(lights[i].position, lights[i].color, LIGHT_RADIUS, 1, 0)
+        );
 
         if (t > 0.0) {
             light_color = lights[i].color * lights[i].intensity;
@@ -113,20 +125,44 @@ bool hit_light(Ray ray, out vec4 light_color) {
     return false;
 }
 
+// The schlick approxmiation and glass scattering was adapted from:
+// https://raytracing.github.io/books/RayTracingInOneWeekend.html
+float schlick(float cos_theta, float ior) {
+    float r0 = (1.0 - ior) / (1.0 + ior);
+    r0 *= r0;
+    return r0 + (1.0 - r0) * pow(1.0 - cos_theta, 5.0);
+}
+
+vec3 glass_scatter(vec3 ray_dir, vec3 hit_point, vec3 normal, float eta, vec2 seed) {
+    vec3 unit_dir = normalize(ray_dir);
+    float cos_theta = min(dot(-unit_dir, normal), 1.0);
+    float sin_theta = sqrt(1.0 - cos_theta * cos_theta);
+
+    bool cannot_refract = eta * sin_theta > 1.0;
+
+    if (cannot_refract || schlick(cos_theta, eta) > rand(seed)) {
+        return reflect(unit_dir, normal);
+    } else {
+        return refract(unit_dir, normal, eta);
+    }
+}
+
 vec4 trace_ray(Ray ray, vec2 seed) {
+    int depth = 4;
     vec4 color = vec4(1.0);
     Ray current = ray;
-    int depth = 128;
 
     for (int bounce = 0; bounce < depth; ++bounce) {
         float closest = 1e20;
         int hit_index = -1;
+        bool hit_plane = false;
 
         for (int i = 0; i < spheres_size; ++i) {
             float t = intersect_sphere(current, spheres[i]);
             if (t > 0.0 && t < closest) {
                 closest = t;
                 hit_index = i;
+                hit_plane = false;
             }
         }
 
@@ -135,33 +171,44 @@ vec4 trace_ray(Ray ray, vec2 seed) {
         if (t_plane > 0.0 && t_plane < closest) {
             closest = t_plane;
             hit_index = -2;
+            hit_plane = true;
         }
 
-        if (closest > 1e19) return color * vec4(0.85, 0.82, 1, 1);
-        // if (closest > 1e19) return vec4(0.01);
+        if (closest > 1e19) return color * vec4(0.85, 0.82, 1.0, 1.0);
+
         vec3 point = current.position + current.direction * closest;
-        vec3 normal = hit_index >= 0 ? normalize(point - spheres[hit_index].center) : plane.normal;
-        color *= hit_index >= 0 ? spheres[hit_index].color : vec4(checkerboard_color(point), 1.0);
+        vec3 normal = hit_plane ? plane.normal : normalize(point - spheres[hit_index].center);
 
-        vec4 light_color;
-        vec3 reflected = reflect(current.direction, normal);
-
-        if (hit_light(Ray(point, current.direction), light_color)) {
-            color *= light_color;
+        if (hit_plane) {
+            color *= vec4(checkerboard_color(point), 1.0);
+        } else {
+            color *= mix(vec4(1.0), spheres[hit_index].color, 0.3);
         }
 
-        vec3 random_dir = normalize(normal + vec3(rand(seed), rand(seed + 1), rand(seed + 2)) - 0.5);
+        vec3 n = normal;
+        float eta = 1.0 / spheres[hit_index].ior;
 
-        current = Ray(point + normal * 1e-4, normalize(mix(reflected, random_dir, 0.3))); // roughness
+        if (dot(current.direction, normal) > 0.0) n *= -1, eta = 1 / eta;
+
+        vec3 new_dir;
+
+        if (hit_plane) {
+            new_dir = reflect(current.direction, normal);
+        } else {
+            vec3 blur = normalize(rand_dir(seed));
+            new_dir = glass_scatter(current.direction, point, n, eta, seed);
+            new_dir = normalize(mix(new_dir, blur, spheres[hit_index].rough));
+        }
+
+        current = Ray(point + n * 1e-4, new_dir);
         seed += 0.1;
-
     }
 
     return color;
 }
 
 void main() {
-    int samples = 128;
+    int samples = 8;
     vec4 sum = vec4(0.0);
 
     for (int i = 0; i < samples; ++i) {
